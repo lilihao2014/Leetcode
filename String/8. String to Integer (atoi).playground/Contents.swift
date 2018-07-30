180f2cff3580324612318059df5e724bf2dc665f
lilihao2014 <lilihao2014@hotmail.com>
2018-07-30 00:31:01 -0700

    8. String to Integer (atoi)

---
+++ String/8. String to Integer (atoi).playground/Contents.swift
@@ -0,0 +1,49 @@
+class Solution {
+    func myAtoi(_ str: String) -> Int {
+        var res = 0
+        var start = 0
+        if str.isEmpty {
+            return res
+        }
+        
+        
+        let charArray = Array(str)
+        while start < charArray.count && charArray[start] == " " {
+            start += 1
+        }
+        
+        guard start < charArray.count else {
+            return res
+        }
+        
+        var flag = 1
+        if charArray[start] == "+" {
+            start += 1
+        }else if charArray[start] == "-" {
+            start += 1
+            flag = -1
+        }
+        
+        let int_max = 2147483647
+        let int_min = -2147483648
+        
+        while start < charArray.count {
+            if let num = Int(String(charArray[start])) {
+                res = res * 10 + num
+                
+                if res >= int_max {
+                    if flag == 1 {
+                        return int_max
+                    }else if res > int_max && flag == -1 {
+                        return int_min
+                    }
+                }
+                start += 1
+            }else {
+                break
+            }
+        }
+        
+        return res * flag
+    }
+}
