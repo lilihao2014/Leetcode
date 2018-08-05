91674c6585c45efce40932dc7c35a22b42ce23d7
lilihao2014 <lilihao2014@hotmail.com>
2018-08-05 13:15:17 -0700

    617. Merge Two Binary Trees

--- Tree/617. Merge Two Binary Trees.swift
+++ Tree/617. Merge Two Binary Trees.swift
@@ -1,16 +1,29 @@
-public class TreeNode {
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public var val: Int
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
-    }
-}
+//
+//  617. Merge Two Binary Trees.swift
+//  LeetCode_Swift
+//
+//  Created by Lihao Li on 2018/8/5.
+//  Copyright © 2018 Lihao Li. All rights reserved.
+//
 
-class Solution {
-    func mergeTrees(_ t1: TreeNode?, _ t2: TreeNode?) -> TreeNode? {
+import Foundation
+
+class mergeTreesSolution {
+    func mergeTrees1(_ t1: TreeNode?, _ t2: TreeNode?) -> TreeNode? {
+        guard let t1 = t1 else {
+            return t2
+        }
+        guard let t2 = t2 else {
+            return t1
+        }
+        t1.val += t2.val
+        t1.left = mergeTrees1(t1.left, t2.left)
+        t1.right = mergeTrees1(t1.right, t2.right)
+        return t1
+    }
+    
+    
+    func mergeTree2(_ t1: TreeNode?, _ t2: TreeNode?) -> TreeNode? {
         guard let t1 = t1 else {
             return t2
         }
@@ -28,15 +41,15 @@ class Solution {
             if curr[0]?.left == nil {
                 curr[0]?.left = curr[1]!.left
             }else {
-                queue.append([curr[0]?.left, curr[1]?.left])
+                queue.append([curr[0]?.left, curr[1]!.left])
             }
-            
             if curr[0]?.right == nil {
                 curr[0]?.right = curr[1]!.right
             }else {
                 queue.append([curr[0]?.right, curr[1]!.right])
             }
         }
+        
         return t1
     }
 }
