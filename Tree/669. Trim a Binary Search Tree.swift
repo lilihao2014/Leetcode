4c7a9399ac9f27a0b2303e5c86530228f7b43b27
lilihao2014 <lilihao2014@hotmail.com>
2018-08-05 15:40:46 -0700

    669. Trim a Binary Search Tree

--- Tree/669. Trim a Binary Search Tree.swift
+++ Tree/669. Trim a Binary Search Tree.swift
@@ -1,24 +1,25 @@
-public class TreeNode {
-    var left: TreeNode?
-    var right: TreeNode?
-    var val: Int
-    public init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
-    }
-}
+//
+//  669. Trim a Binary Search Tree.swift
+//  LeetCode_Swift
+//
+//  Created by Lihao Li on 2018/8/5.
+//  Copyright © 2018 Lihao Li. All rights reserved.
+//
 
-class Solution {
+import Foundation
+
+class TrimBST {
     func trimBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> TreeNode? {
         guard let root = root else {
             return nil
         }
+        
         if root.val > R {
             return trimBST(root.left, L, R)
         }else if root.val < L {
             return trimBST(root.right, L, R)
         }
+        
         root.left = trimBST(root.left, L, R)
         root.right = trimBST(root.right, L, R)
         return root
