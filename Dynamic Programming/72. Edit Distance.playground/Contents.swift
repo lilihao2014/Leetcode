be075f82a30f84a6f6997bd6c2c70316c05f9b5b
lilihao2014 <lilihao2014@hotmail.com>
2018-08-09 20:11:36 -0700

    72. Edit Distance

---
+++ Dynamic Programming/72. Edit Distance.playground/Contents.swift
@@ -0,0 +1,27 @@
+class MinDistance {
+    func minDistance(_ word1: String, _ word2: String) -> Int {
+        let rows = word1.count + 1
+        let columns = word2.count + 1
+        var res = Array(repeating: Array(repeating: 0, count: columns), count: rows)
+        for i in 0..<rows {
+            res[i][0] = i
+        }
+        for i in 0..<columns {
+            res[0][i] = i
+        }
+        
+        let word1CharArray = Array(word1)
+        let word2CharArray = Array(word2)
+        
+        for i in 1..<rows {
+            for j in 1..<columns {
+                if word1CharArray[i - 1] == word2CharArray[j - 1] {
+                    res[i][j] = res[i - 1][j - 1]
+                }else {
+                    res[i][j] = min(res[i - 1][j - 1], min(res[i - 1][j], res[i][j - 1])) + 1
+                }
+            }
+        }
+        return res[rows - 1][columns - 1]
+    }
+}
