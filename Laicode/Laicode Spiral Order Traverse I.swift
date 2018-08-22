a297d383587eca4952822d315b56d31f8e5a4bef
lilihao2014 <lilihao2014@hotmail.com>
2018-08-21 21:14:53 -0700

    Spiral Order Traverse I

---
+++ Laicode/Laicode Spiral Order Traverse I.swift
@@ -0,0 +1,42 @@
+//
+//  Laicode Spiral Order Traverse I.swift
+//  LeetCode_Swift
+//
+//  Created by Lihao Li on 2018/8/21.
+//  Copyright © 2018 Lihao Li. All rights reserved.
+//
+
+import Foundation
+
+class Spiral {
+    func spiral(_ matrix: [[Int]]) -> [Int] {
+        var res = [Int]()
+        guard matrix.count > 0 && matrix[0].count > 0 else {
+            return res
+        }
+        var startI = 0, endI = matrix.count - 1, startJ = 0, endJ = matrix[0].count - 1
+        while startI <= endI && startJ <= endJ {
+            for i in stride(from: startJ, to: endJ + 1, by: 1) {
+                res.append(matrix[startI][i])
+            }
+            for i in stride(from: startI + 1, to: endI + 1, by: 1) {
+                res.append(matrix[i][endJ])
+            }
+            if startI != endI {
+                for i in stride(from: endJ - 1, to: startJ - 1, by: -1) {
+                    res.append(matrix[endI][i])
+                }
+            }
+            if startJ != endJ {
+                for i in stride(from: endI - 1, to: startI, by: -1) {
+                    res.append(matrix[i][startJ])
+                }
+            }
+            startI += 1
+            endI -= 1
+            startJ += 1
+            endJ -= 1
+        }
+        return res
+    }
+}
