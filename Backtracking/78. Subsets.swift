ff8fc0086635a40cd6659eabc01809575620688c
lilihao2014 <lilihao2014@hotmail.com>
2018-09-09 12:14:05 -0700

    78. Subsets

---
+++ Backtracking/78. Subsets.swift
@@ -0,0 +1,29 @@
+//
+//  78. Subsets.swift
+//  LeetCode_Swift
+//
+//  Created by Lihao Li on 2018/9/9.
+//  Copyright © 2018 Lihao Li. All rights reserved.
+//
+
+import Foundation
+
+class Subsets {
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        var tmp = [Int]()
+        generateSubsets(index: 0, tmp: &tmp, res: &res, nums: nums)
+        return res
+    }
+    
+    
+    private func generateSubsets(index: Int, tmp: inout [Int], res: inout [[Int]], nums: [Int]) {
+        res.append(tmp)
+        
+        for i in index..<nums.count {
+            tmp.append(nums[i])
+            generateSubsets(index: i + 1, tmp: &tmp, res: &res, nums: nums)
+            tmp.removeLast()
+        }
+    }
+}
