b6b5d389a190cbe9c0e66bdf9015a0ae91698263
lilihao2014 <lilihao2014@hotmail.com>
2018-09-18 19:57:59 -0700

    121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock.swift
+++ 121. Best Time to Buy and Sell Stock.swift
@@ -2,22 +2,19 @@
 //  121. Best Time to Buy and Sell Stock.swift
 //  LeetCode_Swift
 //
-//  Created by Lihao Li on 2018/8/6.
+//  Created by Lihao Li on 2018/9/18.
 //  Copyright © 2018 Lihao Li. All rights reserved.
 //
 
 import Foundation
 
-
 class MaxProfit {
     func maxProfit(_ prices: [Int]) -> Int {
-        guard prices.count > 0 else {
-            return 0
-        }
+        guard prices.count > 1 else {return 0}
         var minPrice = prices[0], maxProfit = 0
         for i in 1..<prices.count {
-            maxProfit = max(maxProfit, prices[i] - minPrice)
             minPrice = min(minPrice, prices[i])
+            maxProfit = max(maxProfit, prices[i] - minPrice)
         }
         return maxProfit
     }
