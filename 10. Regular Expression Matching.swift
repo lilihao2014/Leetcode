d5c5162042136a911d8bbe6131081aee16455190
lilihao2014 <lilihao2014@hotmail.com>
2018-09-18 20:43:42 -0700

    10. Regular Expression Matching

--- 10. Regular Expression Matching.swift
+++ 10. Regular Expression Matching.swift
@@ -10,9 +10,9 @@ import Foundation
 
 class IsMatch {
     func isMatch1(_ s: String, _ p: String) -> Bool {
-        if p.isEmpty { return s.isEmpty}
-
-        let isFirstMatch = !s.isEmpty && (p[p.index(p.startIndex, offsetBy: 0)..<p.index(p.startIndex, offsetBy: 1)] == "." || p[p.index(p.startIndex, offsetBy: 0)..<p.index(p.startIndex, offsetBy: 1)] == s[s.index(s.startIndex, offsetBy: 0)..<s.index(s.startIndex, offsetBy: 1)])
+        if p.isEmpty { return s.isEmpty }
+        
+        let isFirstMatch = s.count != 0 && (p[p.index(p.startIndex, offsetBy: 0)..<p.index(p.startIndex, offsetBy: 1)] == "." || p[p.index(p.startIndex, offsetBy: 0)..<p.index(p.startIndex, offsetBy: 1)] == s[s.index(s.startIndex, offsetBy: 0)..<s.index(s.startIndex, offsetBy: 1)])
         
         if p.count >= 2 && p[p.index(p.startIndex, offsetBy: 1)..<p.index(p.startIndex, offsetBy: 2)] == "*" {
             return (isFirstMatch && isMatch1(String(s[s.index(s.startIndex, offsetBy: 1)...]), p)) || isMatch1(s, String(p[p.index(p.startIndex, offsetBy: 2)...]))
