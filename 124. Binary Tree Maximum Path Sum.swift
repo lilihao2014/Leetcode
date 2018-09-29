28185e245679ab70ef83cc250397007a454fb5f5
lilihao2014 <lilihao2014@hotmail.com>
2018-09-28 20:58:07 -0700

    124. Binary Tree Maximum Path Sum

--- 124. Binary Tree Maximum Path Sum.swift
+++ 124. Binary Tree Maximum Path Sum.swift
@@ -2,47 +2,45 @@
 //  124. Binary Tree Maximum Path Sum.swift
 //  LeetCode_Swift
 //
-//  Created by Lihao Li on 2018/8/5.
+//  Created by Lihao Li on 2018/9/28.
 //  Copyright © 2018 Lihao Li. All rights reserved.
 //
 
 import Foundation
+
 
 class MaxPathSum {
     
     var maxValue = Int.min
     
     func maxPathSum(_ root: TreeNode?) -> Int {
-        guard let root = root else {
-            return maxValue
-        }
-        maxValue = root.val
-        _ = getMax(root)
+        getMaxValue(root)
         return maxValue
     }
     
-    private func getMax(_ root: TreeNode?) -> Int {
-        guard let root = root else {
-            return 0
-        }
+    
+    @discardableResult
+    private func getMaxValue(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
         
         var leftValue = Int.min, rightValue = Int.min
         
-        if let _ = root.left {
-            leftValue = getMax(root.left!)
+        if let left = root.left {
+            leftValue = getMaxValue(left)
         }
         
-        if let _ = root.right {
-            rightValue = getMax(root.right)
+        if let right = root.right {
+            rightValue = getMaxValue(right)
         }
         
         leftValue = leftValue > 0 ? leftValue : 0
         rightValue = rightValue > 0 ? rightValue : 0
-    
+        
         if leftValue + rightValue + root.val > maxValue {
             maxValue = leftValue + rightValue + root.val
         }
         
         return max(leftValue, rightValue) + root.val
     }
+    
 }
