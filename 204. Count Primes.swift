2f6cfcf070822eb511ab2a4dd79ccaae650fcd1d
lilihao2014 <lilihao2014@hotmail.com>
2018-11-20 17:39:21 -0800

    204. Count Primes

--- 204. Count Primes.swift
+++ 204. Count Primes.swift
@@ -11,19 +11,22 @@ import Foundation
 
 class CountPrimes {
     func countPrimes(_ n: Int) -> Int {
-        if n <= 2 { return 0 }
-        var res = 1
-        var primesArray = Array(repeating: true, count: n)
-        for i in stride(from: 3, to: n, by: 2) {
-            if primesArray[i - 1] {
-                res += 1
-                var j = 3
+        if n <= 2 {
+            return 0
+        }
+        
+        var isPrimeArray = [Bool](repeating: true, count: n)
+        var maxCount = 0
+        for i in 2..<n {
+            if isPrimeArray[i] {
+                maxCount += 1
+                var j = 2
                 while j * i < n {
-                    primesArray[j * i - 1] = false
-                    j += 2
+                    isPrimeArray[i * j] = false
+                    j += 1
                 }
             }
         }
-        return res
+        return maxCount
     }
 }
