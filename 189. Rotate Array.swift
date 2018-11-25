6c87e4fac6000ad362f9b1072d027ac484f7248e
lilihao2014 <lilihao2014@hotmail.com>
2018-11-24 16:02:50 -0800

    189. Rotate Array

--- 189. Rotate Array.swift
+++ 189. Rotate Array.swift
@@ -12,29 +12,25 @@ import Foundation
 class Rotate {
     func rotate(_ nums: inout [Int], _ k: Int) {
         var left = 0, right = nums.count - 1
-        while left < right {
-            nums.swapAt(left, right)
-            left += 1
-            right -= 1
-        }
         
-        let pivotal = k % nums.count
+        swapNums(&nums, &left, &right)
         
+        let k = k % nums.count
         left = 0
-        right = pivotal - 1
-        while left < right {
-            nums.swapAt(left, right)
-            left += 1
-            right -= 1
-        }
+        right = k - 1
+        swapNums(&nums, &left, &right)
         
-        left = pivotal
+        left = k
         right = nums.count - 1
+        swapNums(&nums, &left, &right)
+    }
+    
+    
+    private func swapNums(_ nums: inout [Int], _ left: inout Int, _ right: inout Int) {
         while left < right {
             nums.swapAt(left, right)
             left += 1
             right -= 1
         }
-        
     }
 }
