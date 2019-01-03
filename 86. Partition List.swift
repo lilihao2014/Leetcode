c75e04d3ee328a347f0d418427fdca739af704dc
lilihao2014 <lilihao2014@hotmail.com>
2019-01-03 10:09:05 -0800

    86. Partition List

--- 86. Partition List.swift
+++ 86. Partition List.swift
@@ -11,21 +11,27 @@ import Foundation
 
 class PartitionList {
     func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
-        let leftDummy = ListNode(0), rightDummy = ListNode(0)
-        var left = leftDummy, right = rightDummy
         var head = head
+        var leftHead = ListNode(0)
+        let leftDummy = leftHead
+        
+        var rightHead = ListNode(0)
+        let rightDummy = rightHead
+        
         while head != nil {
             if head!.val < x {
-                left.next = head
-                left = left.next ?? ListNode(0)
+                leftHead.next = head
+                leftHead = leftHead.next!
             }else {
-                right.next = head
-                right = right.next ?? ListNode(0)
+                rightHead.next = head
+                rightHead = rightHead.next!
             }
-            head = head!.next
+            head = head?.next
         }
-        right.next = nil
-        left.next = rightDummy.next
+        
+        leftHead.next = rightDummy.next
+        rightHead.next = nil
+        
         return leftDummy.next
     }
 }
